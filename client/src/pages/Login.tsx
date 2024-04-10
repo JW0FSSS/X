8c5508459f3afcf888dd002725bdf49719f22acd
Jose <dstryr77@gmail.com>
2024-04-10 09:06:41 -0500

    update loading in loginPage

--- client/src/pages/Login.tsx
+++ client/src/pages/Login.tsx
@@ -13,30 +13,15 @@ export default function Login() {
 
     const user=useSelector((state:RootState)=>state.user)
     const [loading,setLoading]=useState(true)
-    const [time,setTime]=useState(50)
-
-    useEffect(()=>{
-    
-      const timer=setInterval(()=>{
-        setTime(time-1)
-      },1000)
-      
-    if (time<1) clearInterval(timer)
-
-      return ()=>clearInterval(timer)
-
-    },[time])
 
     useEffect(()=>{
         fetch(URL+"/ping")
         .then((res)=>res.json())
-        .then(data=>setLoading(false))
-        .catch(e=>setLoading(false))
+        .finally(()=>setLoading(false))
     },[])
 
 
-  if (loading&&time<1) return <div className='grid place-content-center h-screen w-full'><h1 className='text-center text-white w-96 text-wrap text-2xl '>Loading...</h1></div>
-  if (loading) return <div className='grid place-content-center h-screen w-full'><h1 className='text-center text-white w-96 text-wrap text-2xl '>Currently, I'm using the free level of render, so you have to wait {time} seconds or so...</h1></div>
+  if (loading) return <div className='grid place-content-center h-screen w-full'><h1 className='text-center text-white w-96 text-wrap text-2xl '>Loading...</h1></div>
 
   return (
     <section className='bg-black text-white w-screen h-full' id="square">
