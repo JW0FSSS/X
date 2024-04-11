d5955a1bc9ff9c98e25b1b5e0fe375fab4ee8efa
Jose <dstryr77@gmail.com>
2024-04-11 10:34:28 -0500

    change url

--- client/src/components/Home/SideBar.tsx
+++ client/src/components/Home/SideBar.tsx
@@ -68,9 +68,9 @@ export function SideBar() {
                 </div>
                 <div className={`${ismodalOpen?"":"hidden"} bg-blue-400/10 bg-opacity-70 backdrop-blur-[3px] fixed top-0 right-0 left-0 z-50 w-full md:inset-0 max-w-full max-h-full`}>
                     <div className="relative p-4 w-full top-20 left-1/3 ">
-                     <form onSubmit={handleSubmit} className="min-w-[400px] max-w-[600px] rounded-xl bg-black text-white fixed z-50 top-20 left-1/3 px-5 pt-14 pb-5" >
+                     <form onSubmit={handleSubmit} className="min-w-[500px] max-w-[600px] rounded-xl bg-black text-white fixed z-50 top-20 left-1/3 px-5 pt-14 pb-5" >
                         <button type="button" className="absolute top-3 left-5 px-2 hover:bg-opacity-50 hover:bg-white/20 rounded-full" onClick={()=>setModal(!ismodalOpen)}>X</button>
-                        <div className="flex gap-4 flex-wrap border-b-[1px] text-xl border-white/30">
+                        <div className="flex gap-4 flex-wrap border-b-[1px] text-xl border-white/30 pb-10">
                             <img src={`${user.image?user.image:"https://www.testhouse.net/wp-content/uploads/2021/11/default-avatar.jpg"}`} alt="" className="rounded-full size-10"/>
                             <input onChange={handleChange} placeholder="¿What is happening?!!"  className=" resize-none bg-transparent w-5/6 focus:outline-none mb-5 "></input>
                             {image?<img src={URL.createObjectURL(image)} alt="" className="rounded-3xl w-10/12 ml-10 mb-5"/>:null}
